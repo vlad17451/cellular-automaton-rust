8f8f87208ffcadd32ec3e2ee6d146dd7cef6e690
vlad17451 <vlad17451@gmail.com>
2024-01-05 14:54:44 +0100

    remove npm

---
+++ build.sh
@@ -0,0 +1,9 @@
+cargo build --features prod --release --target wasm32-unknown-unknown
+
+wasm-bindgen --no-typescript --target web --out-dir ./build/ --out-name "cellular-automaton-rust" ./target/wasm32-unknown-unknown/release/cellular-automaton-rust.wasm
+
+mkdir -p ./build
+
+cp ./public/index.html ./build/index.html
+
+cp -r ./assets ./build
